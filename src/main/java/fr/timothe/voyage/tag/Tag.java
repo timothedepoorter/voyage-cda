5b62c9b95c09448dc2986a4555d44288ec9398c6
palikcuiho <>
2024-02-26 16:38:47 +0100

    :sparkles: ajout de la relation Tag - Hebergement

--- src/main/java/fr/timothe/voyage/tag/Tag.java
+++ src/main/java/fr/timothe/voyage/tag/Tag.java
@@ -3,12 +3,16 @@ package fr.timothe.voyage.tag;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import fr.timothe.voyage.hebergement.Hebergement
 @Getter
 @Setter
 @NoArgsConstructor
@@ -19,5 +23,8 @@ public class Tag {
     @GeneratedValue
     private Integer id;
     private String nom;
+
+    @ManyToMany(mappedBy="tags")
+    private List<Hebergement> hebergements = new ArrayList<>();
 
 }
