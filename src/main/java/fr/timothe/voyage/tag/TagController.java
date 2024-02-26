82b49b1adce5cc1da5f9ee3836735cb4928a8dfc
Juliette117 <ajuliette7@gmail.com>
2024-02-26 16:32:12 +0100

    feat/ ajout CRUD :sparkles:

--- src/main/java/fr/timothe/voyage/tag/TagController.java
+++ src/main/java/fr/timothe/voyage/tag/TagController.java
@@ -1,7 +1,45 @@
 package fr.timothe.voyage.tag;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/tags")
 public class TagController {
+
+    private final TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    //GET
+    @GetMapping
+    public List<Tag> findAll() {
+        return tagService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Tag findById(@PathVariable Integer id) {
+        return tagService.findById(id);
+    }
+
+    //POST
+    @PostMapping
+    public Tag save(@RequestBody Tag tag) {
+        return tagService.save(tag);
+    }
+
+    //PUT
+    @PutMapping("/{id")
+    public Tag update(@RequestBody Tag tag) {
+        return tagService.update(tag);
+    }
+
+    //DELETE
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        tagService.deleteById(id);
+    }
 }
