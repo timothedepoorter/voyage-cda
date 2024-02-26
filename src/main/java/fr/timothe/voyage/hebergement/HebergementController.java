f03b79e112e08b41cf2dd4db4e66fecbf51dabc7
palikcuiho <>
2024-02-26 14:53:53 +0100

    :sparkles: ajout de l'entité Hébergement

---
+++ src/main/java/fr/timothe/voyage/hebergement/HebergementController.java
@@ -0,0 +1,49 @@
+package fr.timothe.voyage.hebergement;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hebergement")
+public class HebergementController {
+
+    private HebergementService hebergementService;
+
+    public HebergementController(HebergementService hebergementService){
+        this.hebergementService = hebergementService;
+    }
+
+    //Create
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Hebergement save(@RequestBody Hebergement hebergement) {
+        return hebergementService.save(hebergement);
+    }
+
+    //Read
+    @GetMapping
+    public List<Hebergement> findAll(){
+        return hebergementService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Hebergement findById(@PathVariable Integer id){
+        return hebergementService.findById(id);
+    }
+
+
+    //Update
+    @PutMapping("/{id}")
+    public Hebergement update(@RequestBody Hebergement hebergement){
+        return hebergementService.update(hebergement);
+    }
+
+
+    //Delete
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id){
+        hebergementService.deleteById(id);
+    }
+}
