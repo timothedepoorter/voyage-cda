5575c09cce05ebc9a19eec70288c535f954b7aff
Juliette117 <ajuliette7@gmail.com>
2024-02-26 15:06:01 +0100

    feat/ ajout de la classe Tag :heavy_plus_sign:

---
+++ src/main/java/fr/timothe/voyage/tag/TagRepository.java
@@ -0,0 +1,8 @@
+package fr.timothe.voyage.tag;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+}
---
+++ src/main/java/fr/timothe/voyage/tag/TagService.java
@@ -0,0 +1,7 @@
+package fr.timothe.voyage.tag;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TagService {
+}
