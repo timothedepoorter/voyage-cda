46c6aeec3680de94c17ccc9b44797837c194504e
Juliette117 <ajuliette7@gmail.com>
2024-02-26 15:58:52 +0100

    feat/ ajout CRUD :sparkles:

--- src/main/java/fr/timothe/voyage/vol/VolController.java
+++ src/main/java/fr/timothe/voyage/vol/VolController.java
@@ -1,7 +1,44 @@
 package fr.timothe.voyage.vol;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/vols")
 public class VolController {
+    private final VolService volService;
+
+    public VolController(VolService volService) {
+        this.volService = volService;
+    }
+
+    //GET
+    @GetMapping
+    public List<Vol> findAll() {
+        return volService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Vol findById(@PathVariable Integer id) {
+        return volService.findById(id);
+    }
+
+    //POST
+    @PostMapping
+    public Vol save(@RequestBody Vol vol) {
+        return volService.save(vol);
+    }
+
+    //PUT
+    @PutMapping("/{id")
+    public Vol update(@RequestBody Vol vol) {
+        return volService.update(vol);
+    }
+
+    //DELETE
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        volService.deleteById(id);
+    }
 }
--- src/main/java/fr/timothe/voyage/vol/VolService.java
+++ src/main/java/fr/timothe/voyage/vol/VolService.java
@@ -2,6 +2,41 @@ package fr.timothe.voyage.vol;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class VolService {
+
+    private final VolRepository volRepository;
+
+    public VolService(VolRepository volRepository) {
+        this.volRepository = volRepository;
+    }
+
+
+    public List<Vol> findAll() {
+        return volRepository.findAll();
+    }
+
+    public Vol findById( Integer id) {
+        return  volRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Aucun vol ne correspond")
+        );
+    }
+
+
+    public Vol save(Vol vol) {
+        return volRepository.save(vol);
+    }
+
+
+    public Vol update(Vol vol) {
+        return volRepository.save(vol);
+    }
+
+
+    public void deleteById(Integer id) {
+        Vol vol = this.findById(id);
+        volRepository.delete(vol);
+    }
 }
