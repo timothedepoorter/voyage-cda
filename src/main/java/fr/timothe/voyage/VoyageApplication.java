d0076fa2f8f79d7b3480594528044a274bf408f1
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-26 13:58:07 +0100

    init project

---
+++ src/main/java/fr/timothe/voyage/VoyageApplication.java
@@ -0,0 +1,13 @@
+package fr.timothe.voyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/timothe/voyage/VoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.timothe.voyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
