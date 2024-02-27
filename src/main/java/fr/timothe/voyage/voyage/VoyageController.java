b9f8de66efcae36b017f517b4acad2a117f1be41
Timothé DEPOORTER <depoortertimothe1@gmail.com>
2024-02-27 10:02:50 +0100

    CRUD voyage

--- src/main/java/fr/timothe/voyage/voyage/VoyageController.java
+++ src/main/java/fr/timothe/voyage/voyage/VoyageController.java
@@ -1,7 +1,8 @@
 package fr.timothe.voyage.voyage;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/voyage")
@@ -10,5 +11,30 @@ public class VoyageController {
 
     public VoyageController(VoyageService voyageService) {
         this.voyageService = voyageService;
+    }
+
+    @GetMapping()
+    public List<Voyage> findAll() {
+        return this.voyageService.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Voyage findById(@PathVariable Integer id) {
+        return this.voyageService.findById(id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable Integer id) {
+        this.voyageService.delete(id);
+    }
+
+    @PostMapping()
+    public Voyage save(Voyage voyage) {
+        return this.voyageService.save(voyage);
+    }
+
+    @PutMapping(path ="/{id}")
+    public Voyage update(Voyage voyage) {
+        return this.voyageService.save(voyage);
     }
 }
