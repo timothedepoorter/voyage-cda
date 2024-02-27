d912d8c9226910f6f74846a0d4fba8c5784b9b42
Arheee <justinewallet1@gmail.com>
2024-02-27 10:52:53 +0100

    crud Pays

--- src/main/java/fr/timothe/voyage/pays/PaysService.java
+++ src/main/java/fr/timothe/voyage/pays/PaysService.java
@@ -1,6 +1,7 @@
 package fr.timothe.voyage.pays;
-
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PaysService {
@@ -8,5 +9,26 @@ public class PaysService {
 
     public PaysService(PaysRepository paysRepository) {
         this.paysRepository = paysRepository;
+    }
+
+    public List<Pays> findAll() {
+        return paysRepository.findAll();
+    }
+
+    public Pays findById(Integer id) {
+        return paysRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Le pays n'existe pas")
+        );
+    }
+
+    public Pays save(Pays pays) {return paysRepository.save(pays);}
+
+    public Pays update(Pays pays){
+        return paysRepository.save(pays);
+    }
+
+    public void deleteById(Integer id){
+        Pays pays = this.findById(id);
+        paysRepository.delete(pays);
     }
 }
