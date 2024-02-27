f708655b356aa3de74991d921dadcbac37484941
Arheee <justinewallet1@gmail.com>
2024-02-27 15:21:26 +0100

    find hebergement ByVille :construction:

--- src/main/java/fr/timothe/voyage/hebergement/HebergementController.java
+++ src/main/java/fr/timothe/voyage/hebergement/HebergementController.java
@@ -1,9 +1,11 @@
 package fr.timothe.voyage.hebergement;
-
-
+import fr.timothe.voyage.ville.Ville;
+import fr.timothe.voyage.ville.VilleService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -11,11 +13,13 @@ import java.util.List;
 public class HebergementController {
 
     private final HebergementService hebergementService;
+    private final VilleService villeService;
 
 
-    public HebergementController(HebergementService hebergementService){
+    public HebergementController(HebergementService hebergementService, VilleService villeService){
         this.hebergementService = hebergementService;
 
+        this.villeService = villeService;
     }
 
     //Create
@@ -26,19 +30,10 @@ public class HebergementController {
     }
 
     //Read
-    @GetMapping
-    public List<Hebergement> findAll(@RequestParam(required = false) Integer id){
-        if (id != null){
-            hebergementService.findAllByVilleId(id);
-        }
-        return hebergementService.findAll();
-    }
-
     @GetMapping("/{id}")
     public Hebergement findById(@PathVariable Integer id){
         return hebergementService.findById(id);
     }
-
 
     //Update
     @PutMapping("/{id}")
@@ -46,10 +41,47 @@ public class HebergementController {
         return hebergementService.update(hebergement);
     }
 
-
     //Delete
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable Integer id){
         hebergementService.deleteById(id);
     }
+
+
+
+//Chercher un hébergement par ...
+
+//Destination
+//http://localhost:8080/hebergement/findByVille?villeId=1
+@GetMapping("/findByVille")
+public List<Hebergement> findByVille(@RequestParam Integer villeId) {
+    Ville ville = villeService.findById(villeId);
+    return hebergementService.findByVille(ville);
+}
+
+//    @GetMapping("/searchByTag")
+//    public ResponseEntity<List<Hebergement>> searchByTag(@RequestParam String tag) {
+//        List<Hebergement> result = hebergementService.searchByTag(tag);
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/searchByTarif")
+//    public ResponseEntity<List<Hebergement>> searchByTarif(@RequestParam double prix) {
+//        List<Hebergement> result = hebergementService.searchByTarif(prix);
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/searchByDateVol")
+//    public ResponseEntity<List<Hebergement>> searchByDateVol(@RequestParam String dateVol) {
+//        LocalDate parsedDate = LocalDate.parse(dateVol);
+//        List<Hebergement> result = hebergementService.searchByDateVol(parsedDate);
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/searchByPlacesDisponibles")
+//    public ResponseEntity<List<Hebergement>> searchByPlacesDisponibles(@RequestParam Integer placesDisponibles) {
+//        List<Hebergement> result = hebergementService.searchByPlacesDisponibles(placesDisponibles);
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
+
 }
--- src/main/java/fr/timothe/voyage/hebergement/HebergementRepository.java
+++ src/main/java/fr/timothe/voyage/hebergement/HebergementRepository.java
@@ -1,11 +1,17 @@
 package fr.timothe.voyage.hebergement;
 
+import fr.timothe.voyage.ville.Ville;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface HebergementRepository extends JpaRepository<Hebergement, Integer> {
-    Optional<List<Hebergement>> findAllByVilleId(Integer id);
+    List<Hebergement> findByVille(Ville ville);
+//    List<Hebergement> findByTags(String tag);
+//    List<Hebergement> findByTarif(double prix);
+//    List<Hebergement> findByPlacesDisponibles(int placesTotal);
+//    List<Hebergement> findByVols_DateVol(LocalDate dateVol);
 
 }
