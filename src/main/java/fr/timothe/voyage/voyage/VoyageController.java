69d25dce0e9bac1486cbec9cb8c10d2e8bb7030b
palikcuiho <>
2024-02-27 12:03:08 +0100

    ajout des s aux routes

--- src/main/java/fr/timothe/voyage/voyage/VoyageController.java
+++ src/main/java/fr/timothe/voyage/voyage/VoyageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/voyage")
+@RequestMapping(path = "/voyages")
 public class VoyageController {
     private final VoyageService voyageService;
 
